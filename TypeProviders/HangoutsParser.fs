8af34e34fc6418d1c6f386a624325109d92308af
vandeman <padraic.duffy.1@gmail.com>
2015-11-28 22:07:41 +0000

    Initial commit

---
+++ TypeProviders/HangoutsParser.fs
@@ -0,0 +1,44 @@
+﻿namespace TypeProviders
+
+open System
+open FSharp.Data
+
+
+    
+type private HangoutsJson = FSharp.Data.JsonProvider<"Hangouts.json">
+
+type MessageRecord = {
+    Message:string
+    Sender:string
+    Timestamp:System.DateTime
+    MessageType:string
+}
+
+
+type HangoutsFileParser() =
+    member this.Parse(stream : IO.Stream) =
+        let root = HangoutsJson.Load(stream)
+        
+        seq { for conversationCore in root.ConversationState do
+                    let participants = conversationCore.ConversationState.Conversation.ParticipantData
+                    for event in conversationCore.ConversationState.Event do
+
+                            let sender = 
+                                participants 
+                                |> Array.filter (fun p -> p.Id.GaiaId = event.SenderId.GaiaId) 
+                                |> Array.exactlyOne
+                                //|> Seq.exactlyOne
+
+                            let message = 
+                                event.ChatMessage.MessageContent.Segment 
+                                |> Array.exactlyOne
+                                //|> Array.toSeq 
+
+                            yield { 
+                                Message = message.Text; 
+                                Sender = sender.FallbackName; 
+                                Timestamp = DateTime.Now; 
+                                MessageType = message.Type 
+                                }
+
+            }
